ae4afdfe8836c0553372f5fbd63133730ff71c78
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-11-17 10:46:11 +0100

    finish

---
+++ app.js
@@ -0,0 +1,10 @@
+//Function update text
+function update(id, texte){
+    document.getElementById(id).innerHTML = texte;
+}
+
+update("p1", "Toss");
+update("p2", "A");
+update("p3", "Coin");
+update("p4", "To");
+update("p5", "Your Witcher");
